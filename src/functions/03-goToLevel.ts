e0b84745f2ef3a66d921d9b9093a57d84a63a183
Sammyhein <sam.pym@gmail.com>
2026-03-02 15:05:28 +0100

    exo 03 done

---
+++ src/functions/03-goToLevel.ts
@@ -0,0 +1,23 @@
+/*
+This function simulates an elevator system.
+
+The elevator starts at floor 0.
+Based on the starting floor and the destination floor,
+it should move up or down accordingly.
+
+The function should determine the direction of movement
+and simulate the elevator reaching the desired floor.
+
+For example: 
+const start = 0
+const end = 3
+goToLevel(startFloor, FinalPosition) => return 3
+
+const start = 3
+const end = 0
+goToLevel(startFloor, FinalPosition) => return -3
+*/
+export const goToLevel = (startFloor : number, finalPosition : number) => {
+    if(startFloor < 0 || finalPosition < 0 || startFloor > 15 || finalPosition > 15) return 0
+    return finalPosition - startFloor
+};
